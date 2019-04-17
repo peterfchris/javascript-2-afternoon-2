e8df9ebe5f1d3e1c292f0f395734461ea9c573e2
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-04-17 16:03:53 -0600

    Completed Problems  1 - 5

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr) {
+  return arr[0]
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr) {
+  return arr[arr.length - 1]
+}
 
 
 
@@ -47,8 +51,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
+function looper(family) {
+  for (var i=0; i < family.length; i++) {
+    alert(family[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -62,8 +69,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
-
+function reversedLooper(letters) {
+  for (var i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -77,8 +87,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
-
+function evenFinder(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i]
+    if (num%2 !== 0) {
+      nums.splice(i, 1)
+    }
+  } return nums 
+}
 
 
 
